8413e37fc70de8f782008844bb88353201160ee8
chema969 <chema969@hotmail.com>
2019-11-04 19:09:46 +0100

    practica5

---
+++ practica5/potencia.m
@@ -0,0 +1,23 @@
+function pote=potencia (c, d, n)
+binario_d=bitget(d,uint64(log2(d))+1:-1:1,'uint64');
+binario_d=flip(binario_d);
+aux=zeros(size(binario_d));
+aux(1)=mod(c,n);
+res=uint64(1);
+res(1)=1;
+if(binario_d(1)==1)
+    res(1)=res(1)*aux(1);
+end
+
+for i=2:length(binario_d)
+    aux(i)=mod(aux(i-1)*aux(i-1),n);
+    if(binario_d(i)==1)
+        res(1)=mod(aux(i)*res(1),n);
+        
+    end
+end
+aux
+pote=res(1);
+
+end
+
