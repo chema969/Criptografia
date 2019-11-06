187c1e0d05b09a7da0fd7cf6f1da32f40c08472c
chema969 <chema969@hotmail.com>
2019-11-06 12:11:29 +0100

    potencia funciona

--- practica5/potencia.m
+++ practica5/potencia.m
@@ -1,22 +1,23 @@
 function pote=potencia (c, d, n)
 binario_d=bitget(d,uint64(log2(d))+1:-1:1,'uint64');
+binario_d
 binario_d=flip(binario_d);
-aux=zeros(size(binario_d));
+aux=uint64(size(binario_d));
 aux(1)=mod(c,n);
 res=uint64(1);
 res(1)=1;
 if(binario_d(1)==1)
     res(1)=res(1)*aux(1);
+    %res(1)
 end
 
 for i=2:length(binario_d)
     aux(i)=mod(aux(i-1)*aux(i-1),n);
     if(binario_d(i)==1)
         res(1)=mod(aux(i)*res(1),n);
-        
+        %aux(i)
     end
 end
-aux
 pote=res(1);
 
 end
